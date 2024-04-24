6a89612c5266c5fa5ec3a3e075f673de1ddfb628
Tochiskool <mrchristian79@yahoo.com>
2024-04-24 10:30:48 +0200

    Foods component is filtered according to price and type
    We can add other filters based on customers request

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react"
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import AfroCards from "./AfroCards";
+import Foods from "./components/Foods";
 function App() {
   return (
     <div>
       <Navbar/>
       <Hero/>
       <AfroCards/>
+      <Foods/>
     </div>
   );
 }
